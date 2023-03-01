a60195dc59db905ee13334e4e17b86268eedf81a
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-03-01 10:51:24 -0300

    Add files via upload

---
+++ estrutura de repetição/atividade-1.js
@@ -0,0 +1,24 @@
+
+// digita 10 numeros e mostre o maior :
+
+
+// let maxnumber = 0;
+
+// for(let i = 0;i < 10; i++){
+//     const number = parseFloat(prompt('Digite um número'));
+
+//     maxnumber = number > maxnumber ? number : maxnumber;
+// }
+
+// document.getElementById('info').innerHTML = `O maior número digitado foi: ${maxnumber}`;
+let n = 0;
+
+for(let n = 0; n < 10; n++){
+    const divisor = parseFloat(prompt);
+    divisao = divisor / n;
+
+    document.getElementById('info').innerHTML = divisao;
+
+
+}
+
---
+++ estrutura de repetição/estrutura-de-repeticao.js
@@ -0,0 +1,50 @@
+
+// Usando o For //
+
+// for(let i = 0; i <10; i++){
+//     console.log(i)
+// }
+// ------------------------//-----------------------------------//
+// Usando o While //
+
+// var i = 0;
+
+// while(i < 10){
+//     console.log(i);
+
+//     i++;
+// }
+// ------------------------//----------------------------------//
+// Usando DO...WHILE //
+
+// let i = 0
+
+// do {
+//     console.log(i);
+//     i++
+// } while (i < 10);
+// -----------------------//----------------------------------//
+
+// FOR...OF //
+
+// let esportes = ['futebol', 'basquete', 'volei', 'skate'];
+
+// for(let esporte of esportes) {
+//     console.log(esporte);
+// }
+
+// -----------------------//----------------------------------//
+
+// FOR...IN //
+
+// let person = {
+//     name : 'john',
+//     age : 31,
+//     weigth: 60
+// };
+
+// for(let property in person){
+//     console.log(person[property])
+// }
+
+// -----------------------//----------------------------------//
